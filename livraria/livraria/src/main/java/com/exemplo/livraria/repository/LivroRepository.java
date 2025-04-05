08eb57a4665556e4ee0162a9441bd3482540a0bc
nxtannnn1 <167127767+nxtannnn1@users.noreply.github.com>
2025-04-04 21:29:58 -0300

    Delete livraria/livraria directory

--- livraria/livraria/src/main/java/com/exemplo/livraria/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exemplo.livraria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.livraria.model.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
